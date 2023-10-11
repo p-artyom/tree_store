a2effd130fa631878f270d51f22a5d31775fb3d7
Pilipenko Artem <121739130+p-artyom@users.noreply.github.com>
2023-10-11 09:30:24 +0800

    Update tree_store.py

--- tree_store.py
+++ tree_store.py
@@ -1,22 +1,55 @@
+from itertools import groupby
+
+ITEMS = [
+    {'id': 1, 'parent': 'root'},
+    {'id': 2, 'parent': 1, 'type': 'test'},
+    {'id': 3, 'parent': 1, 'type': 'test'},
+    {'id': 4, 'parent': 2, 'type': 'test'},
+    {'id': 5, 'parent': 2, 'type': 'test'},
+    {'id': 6, 'parent': 2, 'type': 'test'},
+    {'id': 7, 'parent': 4, 'type': None},
+    {'id': 8, 'parent': 4, 'type': None},
+]
+
+
+def parent(item):
+    return item['parent']
+
+
 class TreeStore:
     def __init__(self, items):
-        pass
+        self.__items = {item['id']: item for item in items}
+        self.__parent = {
+            key: [item['id'] for item in group_items]
+            for key, group_items in groupby(self.__items.values(), parent)
+        }
 
     def get_all(self):
-        pass
+        return list(self.__items.values())
 
-    def get_item(self):
-        pass
+    def get_item(self, find_id):
+        return self.__items.get(find_id)
 
-    def get_children(self):
-        pass
+    def get_children(self, find_id):
+        return list(
+            [
+                self.get_item(parent_id)
+                for parent_id in self.__parent.get(find_id, [])
+            ]
+        )
 
-    def get_all_parents(self):
-        pass
+    def get_all_parents(self, find_id):
+        parent_id = self.get_item(find_id).get('parent')
+        parent = self.get_item(parent_id)
+        if parent is not None:
+            return [parent] + self.get_all_parents(parent['id'])
+        return []
 
 
 if __name__ == '__main__':
-    ITEMS = [
+    ts = TreeStore(ITEMS)
+
+    assert ts.get_all() == [
         {'id': 1, 'parent': 'root'},
         {'id': 2, 'parent': 1, 'type': 'test'},
         {'id': 3, 'parent': 1, 'type': 'test'},
@@ -25,24 +58,25 @@ if __name__ == '__main__':
         {'id': 6, 'parent': 2, 'type': 'test'},
         {'id': 7, 'parent': 4, 'type': None},
         {'id': 8, 'parent': 4, 'type': None},
-    ]
-    ts = TreeStore(ITEMS)
+    ], 'Метод get_all() возвращает некорректное значение!'
 
-    # print(ts.get_all())
-    # [{'id':1,'parent':'root'},{'id':2,'parent':1,'type':'test'},
-    # {'id':3,'parent':1,'type':'test'},{'id':4,'parent':2,'type':'test'},
-    # {'id':5,'parent':2,'type':'test'},{'id':6,'parent':2,'type':'test'},
-    # {'id':7,'parent':4,'type':None},{'id':8,'parent':4,'type':None}]
+    assert ts.get_item(7) == {
+        'id': 7,
+        'parent': 4,
+        'type': None,
+    }, 'Метод get_item() возвращает некорректное значение!'
 
-    # print(ts.get_item(7))
-    # {'id':7,'parent':4,'type':None}
+    assert ts.get_children(4) == [
+        {'id': 7, 'parent': 4, 'type': None},
+        {'id': 8, 'parent': 4, 'type': None},
+    ], 'Метод get_children() возвращает некорректное значение!'
 
-    # print(ts.get_children(4))
-    # [{'id':7,'parent':4,'type':None},{'id':8,'parent':4,'type':None}]
+    assert (
+        ts.get_children(5) == []
+    ), 'Метод get_children() возвращает некорректное значение!'
 
-    # print(ts.get_children(5))
-    # []
-
-    # print(ts.get_all_parents(7))
-    # [{'id':4,'parent':2,'type':'test'},{'id':2,'parent':1,'type':'test'},
-    # {'id':1,'parent':'root'}]
+    assert ts.get_all_parents(7) == [
+        {'id': 4, 'parent': 2, 'type': 'test'},
+        {'id': 2, 'parent': 1, 'type': 'test'},
+        {'id': 1, 'parent': 'root'},
+    ], 'Метод get_all_parents() возвращает некорректное значение!'
